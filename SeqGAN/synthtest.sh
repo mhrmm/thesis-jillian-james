683fde4c2a2fc2fd6eada08b73d2bd943da4b9d5
jiljames <jiljames.reed.edu>
2019-12-03 23:08:16 -0800

    Trying to automate synth tests

--- SeqGAN/synthtest.sh
+++ SeqGAN/synthtest.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Running module_sequence_gan.py synth 10 10 10"
 python3 module_sequence_gan.py synth 10 10 10
-echo "Running module_sequence_gan.py synth 25 25 25"
-python3 module_sequence_gan.py synth 25 25 25
+echo "Running module_sequence_gan.py synth 20 20 20"
+python3 module_sequence_gan.py synth 20 20 20
+echo "Running module_sequence_gan.py synth 30 30 30"
+python3 module_sequence_gan.py synth 30 30 30
+echo "Running module_sequence_gan.py synth 40 40 40"
+python3 module_sequence_gan.py synth 40 40 40
 echo "Running module_sequence_gan.py synth 50 50 50"
 python3 module_sequence_gan.py synth 50 50 50
-echo "Running module_sequence_gan.py synth 75 75 75"
-python3 module_sequence_gan.py synth 75 75 75
-echo "Running module_sequence_gan.py synth 100 100 100"
-python3 module_sequence_gan.py synth 100 100 100
